e50e8471c582bbb02abb3a8d62490a416c12708f
sarika11-06 <sarikaprajapat8268@gmail.com>
2023-07-06 15:14:54 +0530

    todo-list

---
+++ src/template/ListTemplate.ts
@@ -0,0 +1,71 @@
+import FullList from "../model/FullList";
+
+interface DOMList {
+  ul: HTMLUListElement;
+  noListMessage: HTMLDivElement;
+  clear(): void;
+  render(fullList: FullList): void;
+}
+
+export default class ListTemplate implements DOMList {
+  ul: HTMLUListElement;
+  noListMessage: HTMLDivElement;
+
+  static instance: ListTemplate = new ListTemplate();
+
+  private constructor() {
+    this.ul = document.getElementById("listItems") as HTMLUListElement;
+    this.noListMessage = document.getElementById(
+      "noListMessage"
+    ) as HTMLDivElement;
+  }
+
+  clear(): void {
+    this.ul.innerHTML = "";
+  }
+
+  render(fullList: FullList): void {
+    this.clear();
+
+    if (fullList.list.length === 0) {
+      this.noListMessage.style.display = "block";
+      FullList.instance.load();
+    } else {
+      this.noListMessage.style.display = "none";
+
+      fullList.list.forEach((item) => {
+        const li = document.createElement("li") as HTMLLIElement;
+        li.className = "item";
+
+        const check = document.createElement("input") as HTMLInputElement;
+        check.type = "checkbox";
+        check.id = item.id;
+        check.tabIndex = 0;
+        check.checked = item.checked;
+        li.append(check);
+
+        check.addEventListener("change", () => {
+          item.checked = !item.checked;
+          fullList.save();
+        });
+
+        const label = document.createElement("label") as HTMLLabelElement;
+        label.htmlFor = item.id;
+        label.textContent = item.item;
+        li.append(label);
+
+        const button = document.createElement("button") as HTMLButtonElement;
+        button.className = "button";
+        button.textContent = "x";
+        li.append(button);
+
+        button.addEventListener("click", () => {
+          fullList.removeItem(item.id);
+          this.render(fullList);
+        });
+
+        this.ul.append(li);
+      });
+    }
+  }
+}
